94c6d8c8ed92a91bd7394944903a72c2deb41e66
Luo Lei <j-luolei@msn.com>
2022-01-03 17:40:03 +0800

    add more setting to the svg file

--- webapi/python_repos.py
+++ webapi/python_repos.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import ProxyError
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
@@ -29,13 +30,24 @@ print('Description:', repo_dict['description'])
 
 #print(response_dict.keys())
 
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24 
+my_config.label_font_size = 14 
+my_config.major_label_font_size = 18 
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+
 names, stars = [], []
 for temp_repo_dict in repo_dicts:
     names.append(temp_repo_dict['name'])
     stars.append(temp_repo_dict['stargazers_count'])
 
 my_style = LS('#333366', base_style = LCS)
-chart = pygal.Bar(style= my_style, x_label_rotation=45, show_legend= False)
+chart = pygal.Bar(style= my_style)
 chart.title = 'MOst-Starred Python Projects on GitHub'
 chart.x_labels = names
 
